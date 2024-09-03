eed08a6473cd77d7f8796769769a34438c372848
Mansi123098 <mansinair68107@gmail.com>
2024-09-03 02:01:04 -0700

    Initial commit message

---
+++ backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "mansitasknew"
+    
+    key   = "global/state/terraform.tfstate" 
+    region = "ap-southeast-2"
+    dynamodb_table = "terraform-locks"
+    
+  }
+}
+
